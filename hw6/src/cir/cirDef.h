8eac5c468b0ecc5b5578e6649faef9fa44e749d5
TonyHsu <b02901085@ntu.edu.tw>
2015-01-21 09:30:08 +0800

    hw6 updated

---
+++ hw6/src/cir/cirDef.h
@@ -0,0 +1,35 @@
+/****************************************************************************
+  FileName     [ cirDef.h ]
+  PackageName  [ cir ]
+  Synopsis     [ Define basic data or var for cir package ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2010-2014 LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#ifndef CIR_DEF_H
+#define CIR_DEF_H
+
+#include <vector>
+
+using namespace std;
+
+class CirGate;
+//class CirGateV;
+class CirMgr;
+
+typedef vector<CirGate*>           GateList;
+typedef vector<unsigned>           IdList;
+//typedef vector<CirGateV*>          GateVList;
+
+enum GateType
+{
+   UNDEF_GATE = 0,
+   PI_GATE    = 1,
+   PO_GATE    = 2,
+   AIG_GATE   = 3,
+   CONST_GATE = 4,
+
+   TOT_GATE
+};
+
+#endif // CIR_DEF_H
